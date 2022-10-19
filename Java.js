77f2b4719149fb7e366a24bb951bab8e0b217473
Eliu Rosario <eliurosario@gmail.com>
2022-10-19 03:10:55 -0400

    Added time blocks, time now variable and local storage

--- Java.js
+++ Java.js
@@ -3,7 +3,33 @@
 let currentDay = moment().format('MMMM DD, YYYY');
 $("#currentDay").html(currentDay);
 
+$(function() {
 
+    $(".saveBtn").on("click", function () {
+        var eventInput = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(eventInput, time);
+
+})
+
+var now = moment().hour().minutes();
+
+
+$(".time-block").each(function () {
+    var block = parseInt($(this).attr("id").split("hour")[1]);
+
+
+
+    
+
+
+})
+
+ 
+
+    
+  
 
 
 
