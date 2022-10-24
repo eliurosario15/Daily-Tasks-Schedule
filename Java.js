3eaefed733cca3279b9d8e0e7b3ac1c4fcca51a1
Eliu Rosario <eliurosario@gmail.com>
2022-10-23 22:24:45 -0400

    completed changeColors function

--- Java.js
+++ Java.js
@@ -1,36 +1,35 @@
-//This variable controls the display of today's date//
-
+$(function() {
 
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
+$(".saveBtn").on("click", function () {
+ 
+    var taskInput = $(this).siblings(".description").val();
+    var timeInput = $(this).parent().attr("id");
 
-    $(".saveBtn").on("click", function () {
-        var taskInput = $(this).siblings(".description").val();
-        var timeInput = $(this).parent().attr("id");
 
-        localStorage.setItem(taskInput, timeInput);
 
-    })
-
-function changeColors () {
-
-    const currenTime = moment().hour();
+function changeColors() {
+    
+    const currentTime = moment().hour(); 
 
     $(".time-block").each(function () {
         //We use parseInt in order to not consider decimals//
-        var timeInterval = parseInt($(this).attr("ide").split("hour")[1]);
-
-        if (timeInterval  < currenTime) {
+        var timeInterval = parseInt($(this).attr("id").split("hour")[1]);
+        
+        if (timeInterval < currentTime) {
             $(this).addClass("past");
         }
-        else if (timeInterval === currenTime) {
+
+        else if (timeInterval === currentTime) {
             $(this).addClass("present");
-        }
+         }
+
         else {
             $(this).addClass("future");
         }
     })
 }
-
- 
+changeColors();
+})
