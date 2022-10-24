631016992a38bdad49417cb1ab140b271525d840
Eliu Rosario <eliurosario@gmail.com>
2022-10-23 22:25:51 -0400

    Created local storage for taks and time

--- Java.js
+++ Java.js
@@ -8,7 +8,17 @@ $(".saveBtn").on("click", function () {
     var taskInput = $(this).siblings(".description").val();
     var timeInput = $(this).parent().attr("id");
 
+    localStorage.setItem(timeInput, taskInput);
+})
 
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
 
 function changeColors() {
     
