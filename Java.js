e29e947906274f57483fb49cf088faa084f64d06
Eliu Rosario <eliurosario@gmail.com>
2022-10-23 21:25:08 -0400

    Created a constant to track current time on changeColor function

--- Java.js
+++ Java.js
@@ -3,31 +3,16 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-$(function () {
  
     $(".saveBtn").on("click", function () {
         var taskInput = $(this).siblings(".description").val();
         var timeInput = $(this).parent().attr("id");
 
         localStorage.setItem(taskInput, timeInput);
-    }
 
+    })
 
-
-
-
-
-
-
-
-
-
-
-
- 
-
+function changeColors () {
+    const currenTime = moment().hour();
+}
     
-  
-
-
-
