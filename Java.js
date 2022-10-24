9653f0b1c3f924bdecd201da292a57dc7bb2cde4
Eliu Rosario <eliurosario@gmail.com>
2022-10-23 20:53:25 -0400

    Restarted Java code

--- Java.js
+++ Java.js
@@ -1,30 +1,22 @@
 //This variable controls the display of today's date//
 
-let currentDay = moment().format('MMMM DD, YYYY');
-$("#currentDay").html(currentDay);
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-$(function() {
-
-    $(".saveBtn").on("click", function () {
-        var eventInput = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        localStorage.setItem(eventInput, time);
-
-})
-
-var now = moment().hour().minutes();
-
-
-$(".time-block").each(function () {
-    var block = parseInt($(this).attr("id").split("hour")[1]);
+$(function () {
+ 
 
 
 
-    
 
 
-})
+
+
+
+
+
+
+
 
  
 
